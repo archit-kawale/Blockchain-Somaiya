e3bb1883133cf4cf455b3b0231cc0addf7dbae17
Archit Kawale <archit.kawale@gmail.com>
2023-09-05 08:11:28 +0530

    Blockchain File

---
+++ BlockChain.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class BlockChain {
+    private ArrayList<Block> blockchain;
+    
+    public BlockChain(){
+        blockchain = new ArrayList<Block>();
+        
+}
+}
